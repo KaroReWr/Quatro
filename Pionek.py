9e45ca4acef1a3b59a14d2293f768716e1433eca
[KaroEr] <[jak-anna@wp.pl]>
2020-09-16 20:25:24 +0200

    remove constant values in class Pionek

--- Pionek.py
+++ Pionek.py
@@ -1,9 +1,9 @@
 class Pionek:
     def __init__(self,color, size, shape, is_hole):
-        self.color = 1
-        self.size = 1
-        self.shape = 1
-        self.is_hole = 1
+        self.color = color
+        self.size = size
+        self.shape = shape
+        self.is_hole = is_hole
 
     def __str__(self):
         return f'color:{self.color}, size:{self.size}, shape: {self.shape}, is hole in pionek: {self.is_hole}'
