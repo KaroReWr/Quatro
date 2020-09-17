c836cbf2afc7c1e09cbea9cb00fc942dba489d7d
[KaroEr] <[jak-anna@wp.pl]>
2020-09-17 18:57:10 +0200

    creating set of 16 pawns

--- quatro_plansza.py
+++ quatro_plansza.py
@@ -5,12 +5,28 @@ class Board:
         p1 = Pionek()
         p2 = Pionek()
         self.pionki = []
-        self.pionki.append(p1)
-        self.pionki.append(p2)
+        self.pionki.append(Pionek(0,0,0,0))
+        self.pionki.append(Pionek(0,0,0,1))
+        self.pionki.append(Pionek(0,0,1,1))
+        self.pionki.append(Pionek(0, 1,1,1))
+        self.pionki.append(Pionek(0, 1, 0, 1))
+        self.pionki.append(Pionek(0, 1,1,0))
+        self.pionki.append(Pionek(0, 0, 1, 0))
+        self.pionki.append(Pionek(0, 1, 0, 0))
+        self.pionki.append(Pionek(1, 0, 0, 0))
+        self.pionki.append(Pionek(1, 0, 0, 1))
+        self.pionki.append(Pionek(1, 0, 1, 1))
+        self.pionki.append(Pionek(1, 1, 1, 1))
+        self.pionki.append(Pionek(1, 1, 0, 1))
+        self.pionki.append(Pionek(1, 1, 1, 0))
+        self.pionki.append(Pionek(1, 0, 1, 0))
+        self.pionki.append(Pionek(1, 1, 0, 0))
 
     def __str__(self):
         pionki = ''
         for pion in self.pionki:
             pionki += str(pion) + '\n'
         return pionki
+
+
 
