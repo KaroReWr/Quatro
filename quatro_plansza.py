a2b4ed3b81c53b7ee4b6a55d9bcf4c3715f7ef4c
[KaroEr] <[jak-anna@wp.pl]>
2020-09-27 22:05:49 +0200

    failed attempt to create object with variables instead of location

--- quatro_plansza.py
+++ quatro_plansza.py
@@ -1,17 +1,23 @@
 from Pionek import Pionek
+import copy
 
 class Board:
     def __init__(self):
         self.pionki = []
 
+    def sthg(self):
         for pion in range(0, 16):
             liczba = '{0:04b}'.format(pion)
-            color = int(list(liczba)[0])
-            size = int(list(liczba)[1])
-            shape = int(list(liczba)[2])
-            is_hole = int(list(liczba)[3])
+            liczba_l = list(liczba)
+            liczba_l.copy()
+            color = int(liczba_l[0])
+            size = int(liczba_l[1])
+            shape = int(liczba_l[2])
+            is_hole = int(liczba_l[3])
             pionek = Pionek(color,size,shape, is_hole)
             self.pionki.append(pionek)
+        return self.pionki
+
 
 
     def __str__(self):
@@ -21,6 +27,7 @@ class Board:
         return pionki
 
 a = Board()
-print(a)
+print(a.sthg())
+
 
 
