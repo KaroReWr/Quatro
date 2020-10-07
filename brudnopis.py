f6c320af7f71e1b4829727e8c8de0369e80dce8a
[KaroEr] <[jak-anna@wp.pl]>
2020-10-07 20:11:15 +0200

    loop to check which values are similar where - FAILED

--- brudnopis.py
+++ brudnopis.py
@@ -9,12 +9,48 @@ for i in range(0,3):
     licznik += 1
 
 
-zbior = []
+zbiory = []
 
 for pion in range(0,16):
     liczba = '{0:04b}'.format(pion)
-    zbior.append(list(liczba))
+    #print(liczba)
 
 
 
 
+
+
+
+
+def sthg():
+    zbior = []
+    for pion in range(0, 16):
+        liczba = '{0:04b}'.format(pion)
+        color = int(list(liczba)[0])
+        size = int(list(liczba)[1])
+        shape = int(list(liczba)[2])
+        is_hole = int(list(liczba)[3])
+        pionek = color,size,shape, is_hole
+        zbior.append(pionek)
+    return zbior
+
+
+z = []
+je = []
+d = []
+t = []
+
+pj = [[0, 1, 2], [0, 1, 3], [0, 1], [0, 2, 3], [0, 2], [0, 3], [0], [1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], [], [0, 1], [0, 1, 3], [0, 2], [0, 2, 3], [0], [0, 3], [1, 2]]
+
+for i in pj:
+    for j in i:
+        if j == 0:
+            z.append(pj.index(i))
+        if j == 1:
+            je.append(pj.index(i))
+        if j == 2:
+            d.append(pj.index(i))
+        if j == 3:
+            t.append(pj.index(i))
+
+print(z, je)
